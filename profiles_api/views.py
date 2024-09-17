57803c77491423d95812ab7c8ced844a02ef7419
Emre <emrealikoc2@gmail.com>
2024-09-17 16:17:36 +0300

    paths->path

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
 
@@ -11,5 +16,15 @@ class HelloApiView(APIView):
 
         return Response({'messege': 'Hello!', 'an_apiview': an_apiview})
 
+    def post(self, request):
+        seralizer = self.serializer_class(data=request.data)
 
-# Create your views here.
+        if seralizer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'sa {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
