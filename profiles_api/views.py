9553dd5854e2745408a56d8f0ce73cc9b776393b
Emre <emrealikoc2@gmail.com>
2024-09-17 14:35:38 +0300

    added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+
+        ]
+
+        return Response({'messege': 'Hello!', 'an_apiview': an_apiview})
+
 
 # Create your views here.
