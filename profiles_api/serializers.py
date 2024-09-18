9e005b711781be81f7622e4c8b4f1a3401ce7ebd
Emre <emrealikoc2@gmail.com>
2024-09-18 11:02:56 +0300

    added post, patch ext.

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,4 +2,3 @@ from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
-    
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 from profiles_api import serializers
 
 class HelloApiView(APIView):
@@ -17,9 +18,9 @@ class HelloApiView(APIView):
         return Response({'messege': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
-        seralizer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
-        if seralizer.is_valid():
+        if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'sa {name}'
             return Response({'message': message})
@@ -28,3 +29,12 @@ class HelloApiView(APIView):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
